b287f4f5ff8fdca5edb1bf7319f97f9f0e5edd73
terifan <pinjata69@gmail.com>
2016-11-09 23:52:04 +0100

    update

--- src/org/terifan/bundle/Bundlable.java
+++ src/org/terifan/bundle/Bundlable.java
@@ -8,4 +8,18 @@ public interface Bundlable
 	void readExternal(Bundle aBundle) throws IOException;
 
 	void writeExternal(Bundle aBundle) throws IOException;
+
+	public default <T extends Bundlable> T unmarshal(String aData)
+	{
+		try
+		{
+			Bundle bundle = new Bundle().unmarshalPSON("{\"dummy\":" + aData + "}").getBundle("dummy");
+			readExternal(bundle);
+			return (T)this;
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
 }
