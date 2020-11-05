83d65d4e69d9e01030be26c2c21111a51ba05ca9
terifan <pinjata69@gmail.com>
2020-11-05 17:22:24 +0100

    update

--- src/org/terifan/bundle/BundleInput.java
+++ src/org/terifan/bundle/BundleInput.java
@@ -6,18 +6,24 @@ public class BundleInput
 	private Container mContainer;
 
 
-	public BundleInput(Container aContainer)
+	BundleInput(Container aContainer)
 	{
 		mContainer = aContainer;
 	}
 
 
+	/**
+	 * Returns the input provided to an readExternal method as an Array.
+	 */
 	public Array array()
 	{
 		return (Array)mContainer;
 	}
 
 
+	/**
+	 * Returns the input provided to an readExternal method as a Bundle.
+	 */
 	public Bundle bundle()
 	{
 		return (Bundle)mContainer;
--- src/org/terifan/bundle/BundleOutput.java
+++ src/org/terifan/bundle/BundleOutput.java
@@ -17,26 +17,51 @@ public class BundleOutput
 	}
 
 
-	public void array(Object... aValues)
+	/**
+	 * Sets the output produced by a writeExternal method to an Array. The output can only be set once.
+	 */
+	public Array array(Object... aValues)
 	{
 		if (mContainer != null)
 		{
 			throw new IllegalStateException("Output already set");
 		}
 		mContainer = Array.from(aValues);
+		return (Array)mContainer;
 	}
 
 
-	public void bundle(Bundle aBundle)
+	/**
+	 * Sets the output produced by a writeExternal method to an Array. The output can only be set once.
+	 */
+	public Array array(Array aArray)
+	{
+		if (mContainer != null)
+		{
+			throw new IllegalStateException("Output already set");
+		}
+		mContainer = aArray;
+		return aArray;
+	}
+
+
+	/**
+	 * Sets the output produced by a writeExternal method to a Bundle. The output can only be set once.
+	 */
+	public Bundle bundle(Bundle aBundle)
 	{
 		if (mContainer != null)
 		{
 			throw new IllegalStateException("Output already set");
 		}
 		mContainer = aBundle;
+		return aBundle;
 	}
 
 
+	/**
+	 * Sets the output produced by a writeExternal method to a Bundle. The output can only be set once.
+	 */
 	public Bundle bundle()
 	{
 		if (mContainer != null)
